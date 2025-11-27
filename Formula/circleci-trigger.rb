7ddf7fff782f5ee11c9a60bc71c12539634322db
Rajesh Karunakaran <84339500+rajeshfintech@users.noreply.github.com>
2025-11-27 13:17:26 -0800

    Update formula to v0.2.2

--- Formula/circleci-trigger.rb
+++ Formula/circleci-trigger.rb
@@ -3,20 +3,20 @@ class CircleciTrigger < Formula
 
   desc "CircleCI trigger CLI"
   homepage "https://github.com/rajeshfintech/circleci-trigger"
-  url "https://github.com/rajeshfintech/circleci-trigger/releases/download/v0.2.1/circleci_trigger-0.2.1.tar.gz"
-  sha256 "33b2dee7afdb47f1fa5525df48a393040db0ded385219602d7e6d558fa1b9cc0"
+  url "https://github.com/rajeshfintech/circleci-trigger/releases/download/v0.2.2/circleci_trigger-0.2.2.tar.gz"
+  sha256 "396beebc748ada053d63a0783d0d21dcf045c3ac34c4439afdbfe4c2bc016080"
   license "MIT"
 
   depends_on "python@3.12"
 
   resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/95/45/ba8fb6a0e8e5a0d28abc89c6f0ca6c58410820cb38faae5f68539f3e41c0/PyYAML-6.0.2.tar.gz"
-    sha256 "2e4c0f6e38b1d34cbb7a4ca93782bb2b5005fd464729725dcf2d44c3a37f0e6f"
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/90/8e/33cb83bdbfaa9a90bf34e66b4a79e927651c01fb9b31c8ed6dfa8185728a/requests-2.32.3.tar.gz"
-    sha256 "68d7dedbb5c1e5c23bf3a4dd63ee39073d28a3fcc594a5a2175b4832c75b7f03"
+    url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   def install
