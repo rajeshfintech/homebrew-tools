494aad3c9588573f42c8c6ccf5f2585b7e1ae2d0
Rajesh Karunakaran <84339500+rajeshfintech@users.noreply.github.com>
2025-11-27 12:03:22 -0800

    Update formula to v0.2.0

--- Formula/circleci-trigger.rb
+++ Formula/circleci-trigger.rb
@@ -1,10 +1,10 @@
 class CircleciTrigger < Formula
   include Language::Python::Virtualenv
 
-  desc "CLI to trigger CircleCI pipelines for n1-iac and n1-k8s"
+  desc "CircleCI trigger CLI"
   homepage "https://github.com/rajeshfintech/circleci-trigger"
-  url "https://github.com/rajeshfintech/circleci-trigger/releases/download/v0.1.0/circleci_trigger-0.1.0.tar.gz"
-  sha256 "b4a541c913ab235e8f141880ccfefff0b6145202d770ad6f63e22a2e2fccbc1f"
+  url "https://github.com/rajeshfintech/circleci-trigger/releases/download/v0.2.0/circleci_trigger-0.2.0.tar.gz"
+  sha256 "6c3bcc8c0b9f44499e7cbee7a0c3983175b21cb348100dcdc3b4abdedb8aae59"
   license "MIT"
 
   depends_on "python@3.12"
@@ -14,6 +14,6 @@ class CircleciTrigger < Formula
   end
 
   test do
-    system "{bin}/circleci-trigger", "--help"
+    system "#{bin}/circleci-trigger", "--help"
   end
 end
